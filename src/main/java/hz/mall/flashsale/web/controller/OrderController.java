3f99c87084c2ba6565f63cfc6c4745eb61506bbd
HaolinZhong <hz2771@cumc.columbia.edu>
2022-08-12 00:27:50 -0400

    use the submit method of the thread pool to deal with incoming create order requests one by one

--- src/main/java/hz/mall/flashsale/web/controller/OrderController.java
+++ src/main/java/hz/mall/flashsale/web/controller/OrderController.java
@@ -19,11 +19,10 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -38,7 +37,13 @@ public class OrderController {
     private final ItemService itemService;
     private final IntegratedService integratedService;
     private final DecreaseStockProducer decreaseStockProducer;
-    private final ExecutorService executorService;
+    private ExecutorService executorService;
+
+    @PostConstruct
+    public void init(){
+        executorService = Executors.newFixedThreadPool(20);
+
+    }
 
     @PostMapping(value = "/create", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public CommonReturnType createOrder(
@@ -65,21 +70,37 @@ public class OrderController {
             }
         }
 
+        // incoming create order requests will be placed in this queue.
+        Future<Object> future = executorService.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                // init a stock log into db for facilitating data consistency
+                String stockLogId = itemService.initStockLog(itemId, amount);
 
-        // init a stock log into db for facilitating data consistency
-        String stockLogId = itemService.initStockLog(itemId, amount);
+                /**
+                 * place the order by transactional async message
+                 *
+                 * upon sending of the message, producer will execute the transaction of creating an order.
+                 * logic inside ensured final consistency of stock data.
+                 *
+                 * returns a boolean suggesting successful creation of order and commit of transaction
+                 */
+                boolean result = decreaseStockProducer.transactionAsyncReduceStock(user.getId(), itemId, promoId, amount, stockLogId);
 
-        /**
-         * place the order by transactional async message
-         *
-         * upon sending of the message, producer will execute the transaction of creating an order
-         * logic inside ensured final consistency of stock data
-         *
-         * returns a boolean suggesting successful creation of order and commit of transaction
-         */
-        boolean result = decreaseStockProducer.transactionAsyncReduceStock(user.getId(), itemId, promoId, amount, stockLogId);
+                if (!result) throw new BusinessException(BusinessErrEnum.UNKNOW_ERROR, "failed to send create order message");
 
-        if (!result) throw new BusinessException(BusinessErrEnum.UNKNOW_ERROR, "failed to send create order message");
+                return null;
+            }
+        });
+
+        try {
+            future.get();
+        } catch (InterruptedException e) {
+            throw new BusinessException(BusinessErrEnum.UNKNOW_ERROR);
+        } catch (ExecutionException e) {
+            throw new BusinessException(BusinessErrEnum.UNKNOW_ERROR);
+        }
+
 
         return CommonReturnType.builder().status("success").build();
     }
